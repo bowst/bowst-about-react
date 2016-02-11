720383c26e5ce13811820ffacd262877f0491fba
Drew <drew@bowst.com>
2016-02-11 10:41:48 -0500

    Step 3.2 - Adding some style

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -1,6 +1,13 @@
 //this is ES6 syntax, it's essentially equivalent to: var React = require('react')
 import React from 'react';
 
+/*
+Bring in our styles.  Because we've set up the common folder as a module directory in our webpack configuration,
+we can reference code in that folder as if it were in node_modules.  That is, we don't need the relative path (i.e. the './' in front of the path)
+to the code because webpack will automatically search both the node_modules and common directories for paths that match our request
+*/
+import styles from "styles/main.scss";
+
 //Bring in our other components
 import EditNote from "./../components/EditNote.jsx";
 
@@ -9,7 +16,7 @@ import EditNote from "./../components/EditNote.jsx";
 export default class App extends React.Component{
 	render(){
 		return (
-			<div>
+			<div className="container">
 				{/*
 					Since JSX is basically XML, all components can only consist of a single node.
 					That's why I've wrapped everything in a div
--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,9 @@ module.exports = {
 		path: path.join(__dirname, "dist"),//define the path where our files will be generated
 		filename: "[name].bundle.js"//name of the file, [name] will be replaced with key in entry object ('app')
 	},
+	resolve: {
+        modulesDirectories: ["node_modules", "common"]
+    },
 	module: {
 		//tells webpack which loaders to use
 		loaders: [
@@ -23,7 +26,24 @@ module.exports = {
 				exclude: /(node_modules)/,
 				//tell babel loader to cache the results of the loader for faster builds
 				cacheDirectory: true
-		    }
+		    },
+		    /*
+		    	Specify the loaders for SCSS files
+	    	*/
+			{ 
+				test: /\.scss$/, 
+				/*
+					Notice how we specify an array of loaders here.  Webpack will pass the file from the last one specified 
+					to the first one.  Here's how it happens:
+
+					1) The SASS loader compiles our SCSS file to CSS
+
+					2) The CSS loader compiles that CSS into a text string
+
+					3) The style loader uses JS to write that string to a <style> tag on the page
+				*/
+				loaders: ["style", "css", "sass"]
+			}
 		]
 	}
 };
